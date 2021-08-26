48a61e7c794b5d7bc76934c77e757e233d33e0e0
Kolko Valentin <valiant9191@gmail.com>
2021-08-27 00:32:13 +0300

    project progress

---
+++ src/component/projects/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../navigation/navigation'
+
+
+ const Projects = ()=>{
+
+    return(
+        <div>
+            <Navigation/>
+           ... in progress</div>
+    )
+}
+export default Projects;

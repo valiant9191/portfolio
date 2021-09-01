8e1b5c7f573dd7e0442a5b80b661ad49ca057be0
Kolko Valentin <valiant9191@gmail.com>
2021-09-01 23:24:06 +0300

    project card with flip effect fixes & move pictures to folder

--- src/component/projects/Projects.js
+++ src/component/projects/Projects.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import Navigation from '../navigation/navigation'
+import React from "react";
+import Navigation from "../navigation/navigation";
+import "./project.scss";
+import ProjectCard from "./projectCard/projectCard";
+import projectDB from "./projectDB";
+
+const Projects = () => {
+  return (
+    <div className="project">
+      <Navigation />
+      <div className='construction'><h1>... in progress.
+          <br></br>Sorry</h1></div>
+      
+      <h1>Portfolio</h1>
+      <section className="project-section">
+        {projectDB.map((el,id)=><ProjectCard
+        key={id}
+        projectName={el.nameProject}
+        projectImage={el.imgProject}
+        />)}
 
 
- const Projects = ()=>{
-
-    return(
-        <div>
-            <Navigation/>
-           ... in progress</div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 export default Projects;
